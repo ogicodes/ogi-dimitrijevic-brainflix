3713cfa612670096dc499ecbe103d3a5e89ffa53
Ogi Dimitrijevic <ogi@ogidesigns.ca>
2024-05-29 16:35:31 -0400

    finished making the header need to style it next

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,22 @@
 import Image from "../Image/Image"
 import MohanMuruge from '../../assets/images/images/Mohan-muruge.jpg' 
 import Logo from '../../assets/images/logo/BrainFlix-logo.svg'
+import Magnifier from '../../assets/images/icons/search.svg'
+
+
 export default function Header() {
 
     return (
         <>
             <Header class='header'>
-                <a href="#"><img class='header__logo' src={Logo} alt="BrainFlix Logo" ></img></a>
+                <a href="#"><img class='header__logo' src={Logo} alt="BrainFlix Logo"></img></a>
                 <div>
                     <form action="submit" className="header__form">
-                        <button className="header__form__button"></button>
-                        <input type="text" className="header__form__search" />
+                        <button class="header__form__button" src={Magnifier}></button>
+                        <input type="text" placeholder="Search" className="header__form__search" />
                     </form>
                     <Image src={MohanMuruge} alt='Profile Picture of Mohan Muruge' />
                 </div>
-                <button></button>
                 
             </Header>
         </>
