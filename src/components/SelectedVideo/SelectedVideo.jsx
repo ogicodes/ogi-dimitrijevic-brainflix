ea68ccf7fcee5256c3da7b5744743a6562924271
Ogi Dimitrijevic <ogi@ogidesigns.ca>
2024-06-02 16:55:49 -0400

    finished styling the next videos for tablet

--- src/components/SelectedVideo/SelectedVideo.jsx
+++ src/components/SelectedVideo/SelectedVideo.jsx
@@ -8,23 +8,26 @@ export default function SelectedVideo(props){
     const {title, channel, description, image, views, timestamp, likes, comments} = props.selectedVideo
     return (
         <div className="selected">
-            <video poster={image} alt={`picture about ${title}`} className="selected__video" controls/>
-            <h1 className='selected__title'>{title}</h1>
-            <div className="selected__div">
-                <ul className="selected__list">
-                    <div className="selected__subcontainer">
-                        <li className="selected__item selected__item-modifier"><p className="selected__channel">By {channel}</p></li>
-                        <li className="selected__item">{timestamp}</li>
-                    </div>
-                    <div className="selected__subcontainer">
-                        <li className="selected__item"><img className='selected__img' src={eye} alt='like button'></img>{views}</li>
-                        <li className="selected__item"><img className='selected__img' src={like} alt='like button'></img>{likes}</li>
-                    </div>
-                </ul>
-
+            <div className="selected__video-background">
+                <video poster={image} alt={`picture about ${title}`} className="selected__video" controls/>
             </div>
-            <p className="selected__description">{description}</p>
-            <Comments  comments={comments} /> 
+            <div className='selected__content-container'>
+                <h1 className='selected__title'>{title}</h1>
+                <div className="selected__div">
+                    <ul className="selected__list">
+                        <div className="selected__subcontainer">
+                            <li className="selected__item selected__item-modifier"><p className="selected__channel">By {channel}</p></li>
+                            <li className="selected__item">{timestamp}</li>
+                        </div>
+                        <div className="selected__subcontainer">
+                            <li className="selected__item"><img className='selected__img' src={eye} alt='like button'></img>{views}</li>
+                            <li className="selected__item"><img className='selected__img' src={like} alt='like button'></img>{likes}</li>
+                        </div>
+                    </ul>
+                </div>
+                <p className="selected__description">{description}</p>
+                <Comments  comments={comments} /> 
+            </div>
         </div>
     )
 }
